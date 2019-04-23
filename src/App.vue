38b0dd6c80d4450bd15d7e6a17a0e3f703dbabb4
marmar95 <raczek95.m@gmail.com>
2019-04-23 21:14:47 +0200

    nasa api

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="nav">
       <router-link to="/">spacer</router-link>
       <router-link to="/about">about</router-link>
+      <router-link to="/search">search</router-link>
     </div>
     <router-view /> 
   </div>
